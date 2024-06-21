c2de7d0451f596158c9c121f859526fe8018de0d
jujudotlol <160071511+jujudotlol@users.noreply.github.com>
2024-06-20 21:02:45 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -417,4 +417,9 @@ Settings = {
     }
 }
 
+if string.find(identifyexecutor(), "Solara") then
+    getgenv().script_key = script_key
+    getgenv().Settings = Settings
+end
+
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/027eff8159670e56634668e449ef64e0.lua"))()
